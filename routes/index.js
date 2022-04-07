02bb599747e9234e2adb956605d0b84cfd41f712
Ratnadeep Simhadri <rsimhadri@paypal.com>
2022-04-07 16:26:24 -0700

    fix: GET review

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,6 @@ router.post('/checkout', function(req, res, next) {
 });
 
 router.post('/review', function(req, res, next) {
-  console.log(req.body.languageSelection)
   let language = 'en_DE';
   if(req.body.languageSelection)
    language = req.body.languageSelection;
@@ -23,6 +22,11 @@ router.post('/review', function(req, res, next) {
 });
 
 router.get('/review', function(req, res, next) {
+  let language = 'en_DE';
+  if(req.body.languageSelection)
+   language = req.body.languageSelection;
+  let sdkHost = process.env['SDK_HOST'] || `localhost.paypal.com:8443`;
+  // let stageTarget = 'te-alm-66536402202444903746246.qa.paypal.com'
   res.render('review',{
     scriptTarget: `https://${sdkHost}/sdk/js?client-id=B_AIHOqh5hcwGhwwbG8OBqaPaPAIBMwB26mFCFbgna2J76sn_Dg5eH7QbbzxqOY5Bhunuey7D_laCs3dGE&components=legal&buyer-country=DE&locale=${language}&debug=true`
   });
